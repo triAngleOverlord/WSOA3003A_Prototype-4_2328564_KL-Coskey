98e05133de82ae73cc0b2b696e22f9ced47da49a
triAngleOverlord <2328564@students.wits.ac.za>
2024-04-17 20:34:17 +0200

    ordering system completed

--- WSOA3003_Prototype 4_KL Coskey_2328564/Assets/Scripts/Ordering System.cs
+++ WSOA3003_Prototype 4_KL Coskey_2328564/Assets/Scripts/Ordering System.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OrderingSystem : MonoBehaviour
 {
     public List<int> order = new List<int>();
+    public int orderINT;
+    public string orderName;
+
     public int timeWaiting;
-    public GameObject orderSheet;
+    public bool hasOrdered;
+
+    public int option;
+    private GameObject orderSheet;
+    private GameObject nameSheet;
     void Start()
     {
         StartCoroutine(waitingTime());
         int fryTYPE = Random.Range(0, 3); // shoestring, waffle, curly, crinkle 
         order.Add(fryTYPE);
-        int oneSPICE = Random.Range(0, 3); //nothing, salt, pepper, paprika
+        int oneSPICE = Random.Range(0, 6); //nothing, salt, pepper, paprika, chilli , garlic, BBQ
         order.Add(oneSPICE);
-        int twoSPICE = Random.Range(0, 3); //nothing, chilli , garlic, BBQ
-        order.Add(twoSPICE);
         int dippingSAUCE = Random.Range(0, 4);//nothing, tomato , mustard, cheese, mayo
         order.Add(dippingSAUCE);
         int packaging = Random.Range(0, 2);// plate, fry box, bowl
         order.Add(packaging);
         Debug.Log(name);
-        orderSheet = GameObject.Find("OrderSheet");
 
+        orderSheet = GameObject.Find("Order Sheet");
+        nameSheet = GameObject.Find("NameSheet");
+        hasOrdered = false;
     }
 
 
@@ -48,6 +56,89 @@ public class OrderingSystem : MonoBehaviour
 
     public void makeTheOrder()
     {
+        nameSheet.GetComponent<TextMeshProUGUI>().text = transform.name;
+        option = 1;
+        if (hasOrdered == false)
+        {
+            StartCoroutine(nextOption());
+        }
+        else
+            return;
+    }
 
+    public IEnumerator nextOption()
+    {
+        hasOrdered = true;
+        orderSheet.transform.GetChild(option).transform.GetComponent<TextMeshProUGUI>().text = new string(optionsTXT(option, order[orderINT]));
+        option ++;
+        orderINT++;
+        yield return new WaitForSecondsRealtime(1);
+        if (option == order.Count +1)
+        {
+            StopAllCoroutines();
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else
+            StartCoroutine (nextOption());
+    }
+
+    public string optionsTXT (int option, int type)
+    {
+        if (option == 1)
+        {
+            orderName = "Fry Type: ";
+            if (type == 0)
+                orderName += "Shoestring";
+            else if (type == 1)
+                orderName += "Waffle";
+            else if (type == 2)
+                orderName += "Curly";
+            else
+                orderName += "Crinkle";
+        }
+        else if (option == 2)
+        {
+            orderName = "Spice: ";
+            if (type == 0)
+                orderName += "Nothing";
+            else if (type == 1)
+                orderName += "Salt";
+            else if (type == 2)
+                orderName += "Pepper";
+            else if (type == 3)
+                orderName += "Paprika";
+            else if (type == 4)
+                orderName += "Chilli";
+            else if (type == 5)
+                orderName += "Garlic";
+            else
+                orderName += "BBQ";
+        }
+        
+        else if (option == 3)
+        {
+            orderName = "Sauce: ";
+            if (type == 0)
+                orderName += "Nothing";
+            else if (type == 1)
+                orderName += "Tomato";
+            else if (type == 2)
+                orderName += "Mustard";
+            else if (type == 3)
+                orderName += "Cheese";
+            else
+                orderName += "Mayo"; 
+        }
+        else if (option == 4) 
+        { 
+            orderName = "Packaging: ";
+            if (type == 0)
+                orderName += "Plate";
+            else if (type == 1)
+                orderName += "Fry box";
+            else 
+                orderName += "Bowl";
+        }
+        return orderName;
     }
 }
--- WSOA3003_Prototype 4_KL Coskey_2328564/Assets/Scripts/generalUIButtons.cs
+++ WSOA3003_Prototype 4_KL Coskey_2328564/Assets/Scripts/generalUIButtons.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 public class generalUIButtons : MonoBehaviour
 {
     [SerializeField] private GameObject assignedPanel;
+    public bool foodInSauce;
+    public bool submitBTN;
+
+    private void Update()
+    {
+        if (submitBTN == true)
+        {
+            if (foodInSauce == true)
+            {
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(801f, 306f, 0);
+            }
+            else
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(1401f, 306f, 0);
+        }
+    }
 
     public void movePanelInFront()
     {
         assignedPanel.transform.SetAsLastSibling();
         Debug.Log("Moved");
+    }
+
+    public void submitFood()
+    {
+        
     }
 }
